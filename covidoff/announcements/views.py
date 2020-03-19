d5e091ac9735f17e3235c8229bda5e7a521e2f70
André Francisco <fratelli@andres-mbp-2.lan>
2020-03-19 23:09:19 +0000

    Updated README, created LICENSE

--- covidoff/announcements/views.py
+++ covidoff/announcements/views.py
@@ -2,6 +2,8 @@ from django.views import View
 from django.http import JsonResponse
 from announcements.models import Announcement
 from announcements.forms import AnnouncementForm
+from django.core.paginator import Paginator
+from django.shortcuts import render
 import json
 
 class AnnouncementView(View):
@@ -20,9 +22,28 @@ class AnnouncementView(View):
 		if not form.is_valid():
 			return JsonResponse(dict(form.errors.items()), status=422)
 
-		Announcement.objects.create(**{
+		announcement = Announcement.objects.create(**{
 			'content': form.cleaned_data['content'],
 		})
 
+		# Spread the word
+		broadcast_announcement(announcement)
+
 		return JsonResponse({})
 
+	def get(self, request):
+
+		announcements = Announcement.objects.all()
+		paginator = Paginator(announcements, 25)	# TODO hardcoded
+
+		page_num = request.GET.get('page')
+		page_obj = paginator.get_page(page_num)
+
+		return render(request, 'announcement_list.html', {
+			'page_obj': page_obj
+		})
+
+def broadcast_announcement(content):
+
+	# TODO Broadcast announcements here
+	pass
