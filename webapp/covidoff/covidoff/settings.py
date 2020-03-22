e56c067fc4b77d25dd963e1875992070d0a98459
Ubuntu <ubuntu@ip-172-31-4-156.sa-east-1.compute.internal>
2020-03-22 22:21:52 +0000

    nGINX try

--- webapp/covidoff/covidoff/settings.py
+++ webapp/covidoff/covidoff/settings.py
@@ -42,9 +42,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v9b2hl1&yn7n(e#5*mrqf#(n6=bly3q&ee3mnr8-m^3s46ye12'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'access.User'
 
